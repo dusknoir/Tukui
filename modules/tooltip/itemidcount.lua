637df0cefc6d40f153428e414095e0695cd9d2e5
dusknoir <me@dusknoir.net>
2012-09-03 22:24:33 -0400

    merge tooltip tweaks

--- modules/tooltip/itemidcount.lua
+++ modules/tooltip/itemidcount.lua
@@ -1,41 +1,41 @@
-local T, C, L, G = unpack(select(2, ...)) 
+local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, variables; C - config; L - locales
 if C.tooltip.enable ~= true then return end
 
-local function ClearItemID(self)
-	self.TukuiItemTooltip = nil
-end
-T.ClearTooltipItemID = ClearItemID
+GameTooltip:HookScript("OnTooltipCleared", function(self) self.TukuiItemTooltip=nil end)
+GameTooltip:HookScript("OnTooltipSetItem", function(self)
+  if (IsAltKeyDown()) and (TukuiItemTooltip and not self.TukuiItemTooltip and (TukuiItemTooltip.id or TukuiItemTooltip.count)) then
+    local item, link = self:GetItem()
+    local num = GetItemCount(link)
+    local left
+    local right
+    
+    if TukuiItemTooltip.id and link ~= nil then
+      left = "|cFFCA3C3CID|r "..link:match(":(%w+)")
+    end
+    
+    if TukuiItemTooltip.count and num > 1 then
+      right = "|cFFCA3C3C"..L.tooltip_count.."|r "..num
+    end
+            
+    self:AddLine(" ")
+    if left or right then
+      self:AddDoubleLine(left or "", right or "")
+    end
 
-local function ShowItemID(self)
-	if (IsShiftKeyDown() or IsAltKeyDown()) and (TukuiItemTooltip and not self.TukuiItemTooltip and (TukuiItemTooltip.id or TukuiItemTooltip.count)) then
-		local item, link = self:GetItem()
-		local num = GetItemCount(link)
-		local left = ""
-		local right = ""
-		
-		if TukuiItemTooltip.id and link ~= nil then
-			left = "|cFFCA3C3CID|r "..link:match(":(%w+)")
-		end
-		
-		if TukuiItemTooltip.count and num > 1 then
-			right = "|cFFCA3C3C"..L.tooltip_count.."|r "..num
-		end
-				
-		self:AddLine(" ")
-		self:AddDoubleLine(left, right)
-		self.TukuiItemTooltip = 1
-	end
-end
-T.ShowTooltipItemID = ShowItemID
+    if TukuiItemTooltip.id then
+      local _, _, _, _, _, _, _, _, _, texture = GetItemInfo(link)
+      self:AddLine("|cFFCA3C3CTexture|r " .. texture:sub(17))
+    end
 
-GameTooltip:HookScript("OnTooltipCleared", ClearItemID)
-GameTooltip:HookScript("OnTooltipSetItem", ShowItemID)
+    self.TukuiItemTooltip = 1
+  end
+end)
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("ADDON_LOADED")
 f:SetScript("OnEvent", function(_, _, name)
-	if name ~= "Tukui" then return end
-	f:UnregisterEvent("ADDON_LOADED")
-	f:SetScript("OnEvent", nil)
-	TukuiItemTooltip = TukuiItemTooltip or {count=true, id=true}
+  if name ~= "Tukui" then return end
+  f:UnregisterEvent("ADDON_LOADED")
+  f:SetScript("OnEvent", nil)
+  TukuiItemTooltip = TukuiItemTooltip or {count=true,id=true}
 end)
